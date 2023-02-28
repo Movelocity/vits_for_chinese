b81fd615de2ec8fd7c17b3686fb7637e62ead72b
Movelocity <2814771302@qq.com>
2023-02-28 16:59:18 +0800

    .

--- data_utils.py
+++ data_utils.py
@@ -77,7 +77,7 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
         if sampling_rate != self.sampling_rate:
             raise ValueError("{} {} SR doesn't match target {} SR".format(
                 sampling_rate, self.sampling_rate))
-        # audio_norm = audio / self.max_wav_value
+        audio_norm = audio
         audio_norm = audio_norm.unsqueeze(0)
         spec_filename = filename.replace(".wav", ".spec.pt")
         if os.path.exists(spec_filename):
