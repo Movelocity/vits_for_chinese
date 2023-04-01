61980c2e6ebe561ef59852da24a0148eeb42a0d8
Movelocity <2814771302@qq.com>
2023-04-01 21:02:35 +0800

    w

--- models.py
+++ models.py
@@ -103,8 +103,8 @@ class DurationPredictor(nn.Module):
         self.proj = nn.Conv1d(filter_channels, 1, 1)
         self.cond = nn.Linear(embed_dim, in_channels)
 
-    def forward(self, x, x_mask, embed, training=False, noise_scale=1):
-        # noise scale 参数仅作为占位，实际上不使用
+    def forward(self, x, x_mask, w, embed, training=False, noise_scale=1):
+        # w, noise scale 参数仅作为占位，实际上不使用
         x = torch.detach(x)
         x += self.cond(torch.detach(embed)).unsqueeze(-1)
         x = self.conv_1(x * x_mask)
